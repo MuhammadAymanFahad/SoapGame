ff565c98a8f6b31f9ff0c7fa41e44a4c14086c0e
MuhammadAymanFahad <muhammadaymanfahad@gmail.com>
2025-01-25 05:57:21 +0700

    cinemachine setup

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -2,18 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class EnemyMovement : MonoBehaviour
 {
-    private float speed;
-    [SerializeField] private Rigidbody2D playerRigidBody;
-
+    // Start is called before the first frame update
     void Start()
     {
-            
+        
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
         
     }
