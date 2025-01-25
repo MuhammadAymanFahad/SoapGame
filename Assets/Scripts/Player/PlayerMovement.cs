6e92115426741c36f04672067ec292a9a3e301bd
MuhammadAymanFahad <muhammadaymanfahad@gmail.com>
2025-01-25 07:00:17 +0700

    turn around features

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -6,6 +6,9 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
     [SerializeField] private Rigidbody2D playerRigidBody;
+    //[SerializeField] private Animator playerAnim;
+
+    private int facingDirection = 1;
 
     void Start()
     {
@@ -18,6 +21,20 @@ public class PlayerMovement : MonoBehaviour
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
 
+        if(horizontal > 0 && transform.localScale.x < 0 || horizontal < 0 && transform.localScale.x > 0)
+        {
+            flip();
+        }
+
+        //playerAnim.SetFloat("Horizontal", Mathf.Abs(horizontal));
+        //playerAnim.SetFloat("Vertical", Mathf.Abs(vertical));
+
         playerRigidBody.velocity = new Vector2(horizontal, vertical) * speed;
+    }
+
+    void flip()
+    {
+        facingDirection *= -1;
+        transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
     }
 }
